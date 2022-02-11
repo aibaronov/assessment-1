588c3d7a7f8c039ee84db28294305791fa974992
Arthur Baronov <abaronov1234@gmail.com>
2022-02-11 11:49:02 -0700

    Initial Commit for password.js

---
+++ password.js
@@ -0,0 +1,4 @@
+//Welcome user to password validator
+console.log("Welcome to the password validator")
+
+let userPassword = prompt("Please enter a valid password. At least 10 characters, 2 numbers, 2 special characters, 2 uppercase letters.");
